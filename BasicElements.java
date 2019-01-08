65d37ed0b80d51e1d2dcc67f7ecdad8f5f3c1b2f
aryanjoyz <guilaran1820867@mkt.ceu.edu.ph>
2019-01-08 13:28:46 +0800

    add Button

--- BasicElements.java
+++ BasicElements.java
@@ -1,13 +1,18 @@
 import javax.swing.JFrame;
+import javax.swing.JButton;
 
 public class BasicElements{
 
 	public static void main (String args []){
 		JFrame myFrame = new JFrame();
+		JButton myButton = new JButton();
+		myFrame.setSize(900, 600);
+
+		myFrame.setLayout(null);
+		myFrame.add(myButton);
 
 		myFrame.setDefaultCloseOperation(myFrame.EXIT_ON_CLOSE);
 		myFrame.setVisible(true);
-		myFrame.setSize(900, 600);
 		
 		System.out.println("basic java elements");
 	}
