8ab7ea3a727a8a8f8169b2668ce8338d0004d160
aryanjoyz <guilaran1820867@mkt.ceu.edu.ph>
2019-01-08 13:06:38 +0800

    add frame

--- BasicElements.java
+++ BasicElements.java
@@ -1,7 +1,14 @@
+import java.swing.JFrame;
 
 public class BasicElements{
 
 	public static void main (String args []){
+		JFrame myFrame = new Jframe();
+
+		myFrame.setDefaultCloseOperation(myFrame.EXIT_ON_CLOSE);
+		myFrame.setVisble(true);
+		myFrame.setSize(500.600);
+		
 		System.out.println("basic java elements");
 	}
 }
